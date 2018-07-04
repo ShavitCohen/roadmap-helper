18825e6fdc9bb68d0b6457f33a22775d1b10e34c
ShavitCohen <shavit.hit@gmail.com>
2018-07-04 18:07:38 +0300

    :wrench: adding comments

--- api/employees/index.js
+++ api/employees/index.js
@@ -4,10 +4,13 @@ const validate = require('../../common/validator');
 const router = new Router();
 const schema = require('./employees.joi.schema');
 const { getEmployees, getData } = require('./employees.controller');
+
+// e.g: http://localhost:3333/api/employees
 router
   .route('/')
   .get(validate(schema.get), getEmployees);
 
+// e.g: http://localhost:3333/api/employees/data?identifiers[]=Shavit Cohen&identifiers[]=שביט כהן&identifiers[]=shavit@tikalk.com&identifiers[]=shavit cohen
 router
   .route('/data')
   .get(validate(schema.getData), getData);
