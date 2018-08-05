00b125ac02494fa51125913dfef110e2fc7caedc
alonspr <alonsap@gmail.com>
2018-08-05 10:26:43 +0300

    fixed index.js
    added loadash dependency
    updated domains whitelist

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const api = require('./api');
+const _ = require('lodash');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const whitelist = ['http://localhost:3010'];
+const whitelist = ['http://localhost:3010', 'http://localhost:8080'];
 
 app.use(cors({
   origin: (origin, cb) => {
